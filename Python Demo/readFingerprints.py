c2942bae6b932477cff8f3e2fdabb43031ef51d0
sec05 <spencer.ec09@gmail.com>
2024-06-27 10:12:38 -0500

    Removing python

--- Python Demo/readFingerprints.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-def readFingerprints(file: str) -> list[np.matrix]:
-    info = np.genfromtxt(file,max_rows=1,dtype=str,delimiter=";")
-    A = np.genfromtxt(file,skip_header=1,dtype=float)
-    return [info,A]
